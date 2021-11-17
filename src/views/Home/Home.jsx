0d4f52133342854b2bb078eb829fa64cc2c1f496
huaungwj <48433992+huaungwj@users.noreply.github.com>
2021-11-17 18:59:30 +0800

    update home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,7 +4,12 @@ import HomeStyle from "./Home.module.css";
 export default function Home() {
     return (
         <div className={HomeStyle.homeContainer}>
-            <div className={`${HomeStyle.bannerText} aby_container`}>123</div>
+            <div className={`${HomeStyle.bannerBox} aby_container`}>
+                <span className={`ThemeColor ${HomeStyle.bannerText}`}>
+                    Airbnb 爱彼迎{" "}
+                </span>
+                <p>全球 700 万民宿任你挑</p>
+            </div>
         </div>
     );
 }
